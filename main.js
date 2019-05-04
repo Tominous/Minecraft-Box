98d0cbcf212ed0d55c6eeb884e7e9c0f3c3bd0a7
Tominous <18189490+Tominous@users.noreply.github.com>
2019-05-04 11:53:08 +0100

    Update main.js

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ const createWindow = () => {
   mainWindow.loadURL(path.join(__dirname, 'src/views/spa.html'));
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools();
+   mainWindow.webContents.openDevTools();
 
   mainWindow.on('ready-to-show', () => {
     mainWindow.show()
